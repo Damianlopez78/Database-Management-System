e5a6a654b08559db6a6a21bd7b78a0331d10a1b9
Damian Lopez <Damianlopez_78@hotmail.com>
2018-05-01 23:57:30 -0400

    Added config.py to connect to the DB and small change to Admin page

---
+++ Python_Files/configDB.py
@@ -0,0 +1,10 @@
+import pymysql
+
+#this scipt will connect to MySQL work bech database
+#replace password with your password
+#db=yourDBnamehere with the name of the Database
+#then you can call setDB anywhere in this project where we have to use the DB
+def setDB():
+    connection = pymysql.connect(host='localhost',user='root',password='hereyourmysqlworkbenchpassword',db='yourDBnamehere',autocommit=True)
+    cursor = connection.cursor()
+    return cursor
